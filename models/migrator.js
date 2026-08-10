139e2ce53e35d5d4b1366d7cbd95ab3f1c6adb0e
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2026-08-10 14:09:44 -0300

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,52 @@
+import database from "infra/database.js";
+import { resolve } from "node:path";
+import runner from "node-pg-migrate";
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await runner({
+      ...defaultMigrationOptions,
+      dbClient: dbClient,
+    });
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await runner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    return migratedMigrations;
+  } finally {
+    await dbClient?.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
